33a3594f8114a2b8815f2df6593c0b7a309bf88e
Kemal Dwi Heldy Muhammad <21bigbozz09@gmail.com>
2021-08-06 14:28:10 +0700

    dim

--- pages/index.vue
+++ pages/index.vue
@@ -66,12 +66,11 @@
           <p ref="description" class="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum enim omnis qui perferendis autem repellendus sapiente officiis?</p>
           <div class="about-company">
             <div ref="bgButton" class="bg-animated"></div>
-            <span ref="wordButton" >GET TO KNOW US</span>
+            <span ref="wordButton" >GET TO KNOW US + </span>
           </div>
         </div>
       </div>
     </div>
-
     <div class="featured-content">
       <div class="title-container">
         <h1 class="title-featured">
@@ -118,6 +117,34 @@
           </div>
         </div>
       </div>
+      <div class="more-work">
+        <div class="bg-work"></div>
+        <span>VIEW MORE WORK + </span>
+      </div>
+    </div>
+    <div class="more-about">
+       <div class="hero-wrapper">
+        <div class="hero">
+          <h2 ref="greet" class="greet">
+            <span 
+              v-for="(n, i) in moreAbout.split('')"
+              :key="i"
+              class="letter"
+              :style="n === ' ' ? 'margin: 0 0.25rem' : ''"
+            >
+              {{ n }}
+            </span>
+          </h2>
+          <h1 ref="title" class="title">
+            <span 
+              class="word-container"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, cumque, velit atque dicta porro at veniam, inventore unde voluptatibus deserunt adipisci? Ea odio ad officia laudantium distinctio. Quaerat, fugit molestiae?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga similique culpa labore distinctio cupiditate ratione ea
+            </span>
+          </h1>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -133,7 +160,8 @@ export default {
       heroGreet: 'WE ARE REDIAN',
       heroTitle: 'The only digital agency you\'ll need.',
       mediaQuery: null,
-      titleFeatured: 'Featured Work'
+      titleFeatured: 'Featured Work',
+      moreAbout: 'A DIGITAL AGENCY'
     }
   },
   computed: {
@@ -514,10 +542,11 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
-    align-items: flex-start;
+    align-items: center;
     .title-container{
       position: relative;
       display: flex;
+      transform: translateX(-50%);
       flex-direction: column;
       justify-content: flex-start;
       align-items: flex-start;
@@ -609,6 +638,119 @@ export default {
         &:nth-child(even){
           // genap
           margin-left: 1.5rem;
+        }
+      }
+    }
+    .more-work{
+    cursor: pointer;
+    position: relative;
+    margin-top: 1.5rem;
+    height: 2rem;
+    padding: 0 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .bg-work{
+      position: absolute;
+      top: 0;
+      left: 0;
+      background: #F2F2F2;
+      transform: translateX(1rem);
+      width: 2rem;
+      height: 2rem;
+      border-radius: 1rem;
+      transition: 0.25s width;
+    }
+    span{
+      position: relative;
+      font-family: 'Manrope';
+      font-weight: 800;
+      transform: translateX(1rem);
+      letter-spacing: 0.2rem;
+      font-size: 12px;
+      color:black;
+    }
+    &:hover{
+      .bg-work{
+        width: 100%;
+      }
+    }
+    }
+
+  }
+  .more-about{
+    position: relative;
+    width: 100%;
+    padding: 5rem calc(12.5vw + 2rem);
+    background: $primary-color;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    .hero-wrapper {
+      position: relative;
+      width: 100vw;
+      height: calc(100vh - 6rem);
+      height: calc((var(--vh, 1vh) * 100) - 6rem);
+      padding: 6rem 12.5vw 0 12.5vw;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      .hero {
+        position: relative;
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        margin-top: 2rem;
+        justify-content: flex-start;
+        align-items: flex-start;
+        h2.greet {
+          position: relative;
+          font-family: 'Oswald';
+          font-size: 0.65rem;
+          font-weight: 400;
+          color:  white;
+          letter-spacing: 0.4rem;
+          span.letter {
+            display: inline-block;
+            transform: translateX(-24px);
+            opacity: 0;
+          }
+        }
+        h1.title {
+          position: relative;
+          left: 2rem;
+          font-family: 'Manrope';
+          font-size: 36px;
+          font-weight: 800;
+          opacity: 0;
+          margin: 1rem 0;
+          line-height: 1.2;
+          color: #fff;
+          span.word-container {
+            position: relative;
+            margin-right: 0.65rem;
+            transform: translateY(1.5rem);
+            padding: 0.125rem 0;
+            overflow: hidden;
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            span.overlay {
+              position: absolute;
+              width: 100%;
+              height: 100%;
+              background: $background-color;
+            }
+            &:first-child {
+              margin-left: 0;
+            }
+            &:last-child {
+              margin-right: 0;
+            }
+          }
         }
       }
     }
