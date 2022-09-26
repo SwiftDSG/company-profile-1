224c481bc05d44400cdfc8f5c8c8e93ad12eb664
Kemal Dwiheldy <33623566+SwiftDSG@users.noreply.github.com>
2022-09-26 22:21:34 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'company-profile-2',
+      name: 'company-profile-1',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
