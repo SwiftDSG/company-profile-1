84ac346288bf9b442bd7bc1ee71982071fc478f4
Kemal Dwiheldy <33623566+SwiftDSG@users.noreply.github.com>
2022-09-28 20:32:33 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,11 @@ module.exports = {
       name: 'company-profile-1',
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs'
+      script: './.output/server/index.mjs',
+      env: {
+        "NUXT_APP_BASE_URL": "/",
+        "PORT": 3000
+      }
     }
   ]
 }
